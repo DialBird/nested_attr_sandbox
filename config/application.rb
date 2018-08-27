b143cb3d601b54f85ee7bbe1523628ff3b25dc79
Keisuke Taniguchi <rememberthatk@icloud.com>
2018-08-27 04:23:21 +0000

    config/appilcation.rbの設定

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,12 @@ module NestedAttrSandbox
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.stylesheets false
+      g.helper false
+      g.decorator false
+      g.template_engine :slim
+      g.test_framework nil
+    end
   end
 end
