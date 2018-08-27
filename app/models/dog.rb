a215d25dd760e5c8cd836f77ca25fd9569bd265b
Keisuke Taniguchi <rememberthatk@icloud.com>
2018-08-27 04:29:05 +0000

    UserとDogを関連づける

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,5 @@
 class Dog < ApplicationRecord
+  ATTRIBUTES = %i[id _destroy name].freeze
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  ATTRIBUTES = %i[name].freeze
+
+  has_many :dogs, dependent: :destroy, inverse_of: :user
+  accepts_nested_attributes_for :dogs, allow_destroy: true, reject_if: :all_blank
 end
